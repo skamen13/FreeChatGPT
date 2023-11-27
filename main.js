66a51c0acbcbb58efcac903adbe78d1607361874
skamen13 <137099249+skamen13@users.noreply.github.com>
2023-11-27 21:17:40 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Путь к статическим файлам
+app.use(express.static(path.join(__dirname, 'public')));
+
+const endpoint =
+    "https://us-central1-chat-for-chatgpt.cloudfunctions.net/basicUserRequestBeta";
+
+function sendResponse(res, status, message) {
+    res.setHeader("Content-Type", "application/json");
+    res.status(status).send(JSON.stringify({ status, message }, null, 2));
+}
+
+app.get("/", async (req, res) => {
+    const text = 'кто ты такой?';
+
+    if (!text) {
+        sendResponse(res, 400, "Please enter text parameter");
+        return;
+    }
+
+    try {
+        const response = await axios.post(
+            endpoint,
+            {
+                data: {
+                    message: text,
+                },
+            },
+            {
+                headers: {
+                    Host: "us-central1-chat-for-chatgpt.cloudfunctions.net",
+                    Connection: "keep-alive",
+                    Accept: "*/*",
+                    "User-Agent":
+                        "com.tappz.aichat/1.2.2 iPhone/16.3.1 hw/iPhone12_5",
+                    "Accept-Language": "en",
+                    "Content-Type": "application/json; charset=UTF-8",
+                },
+            }
+        );
+
+        const result = response.data.result.choices[0].text;
+        sendResponse(res, 200, result);
+    } catch (error) {
+        sendResponse(res, 403, "Error connecting to openai");
+    }
+});
+
+app.post("/", async (req, res) => {
+    const text = req.body.text;
+
+    if (!text) {
+        sendResponse(res, 400, "Please enter text parameter");
+        return;
+    }
+
+    try {
+        const response = await axios.post(
+            endpoint,
+            {
+                data: {
+                    message: text,
+                },
+            },
+            {
+                headers: {
+                    Host: "us-central1-chat-for-chatgpt.cloudfunctions.net",
+                    Connection: "keep-alive",
+                    Accept: "*/*",
+                    "User-Agent":
+                        "com.tappz.aichat/1.2.2 iPhone/16.3.1 hw/iPhone12_5",
+                    "Accept-Language": "en",
+                    "Content-Type": "application/json; charset=UTF-8",
+                },
+            }
+        );
+
+        const result = response.data.result.choices[0].text;
+        sendResponse(res, 200, result);
+    } catch (error) {
+        sendResponse(res, 403, "Error connecting to openai");
+    }
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    sendResponse(res, 500, "Something broke!");
+});
+
+app.listen(3000, () => {
+    console.log("ChatGPT API is running on port 3000");
+});
+
